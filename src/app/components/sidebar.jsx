36b9d1a077df7a392378418d999a06d8deda90f7
Sardar-Taha <tahaabb1122@gmail.com>
2023-11-05 13:42:35 +0500

    Active effect added

--- src/app/components/sidebar.jsx
+++ src/app/components/sidebar.jsx
@@ -12,12 +12,14 @@ import {
 } from "../assets";
 import { useRouter } from "next/navigation";
 import { AiFillHome } from "react-icons/ai";
+import { usePathname } from "next/navigation";
 
 function Sidebar() {
   const router = useRouter();
+  const pathname = usePathname();
 
   return (
-    <div className="w-full bg-secondary h-screen overflow-hidden">
+    <div className="w-full bg-secondary h-screen overflow-hidden px-5">
       <Image
         src={sidebarLogo}
         alt="Sidebar Logo"
@@ -25,28 +27,40 @@ function Sidebar() {
       />
 
       <div
-        className="flex items-center ml-[2.5rem] gap-4  mt-10 cursor-pointer"
+        className={`flex items-center gap-4  mt-4 cursor-pointer py-4 px-2 ${
+          pathname === "/" ? "bg-white rounded-lg" : ""
+        }`}
         onClick={() => router.push("/")}
       >
-        <AiFillHome className="text-[#fff] text-2xl mb-1" />
-        <h1 className="text-[16px] font-normal text-[#fff]  ">Home</h1>
+        <AiFillHome
+          className={`text-[#fff] text-2xl mb-1 ${
+            pathname === "/" ? "text-secondary" : ""
+          }`}
+        />
+        <h1
+          className={`text-[16px] font-normal text-[#fff] ${
+            pathname === "/" ? "text-secondary" : ""
+          }`}
+        >
+          Home
+        </h1>
       </div>
       <div
-        className="flex ml-[2.5rem] gap-4  mt-10 cursor-pointer"
+        className="flex gap-4  mt-4 cursor-pointer py-4 px-2"
         onClick={() => router.push("/reports")}
       >
         <Image src={reportIcon} alt="Home Icon" className="h-[22px]" />
         <h1 className="text-[16px] font-normal text-[#fff]  ">Reports</h1>
       </div>
       <div
-        className="flex ml-[2.5rem] gap-4  mt-10 cursor-pointer"
+        className="flex gap-4  mt-4 cursor-pointer py-4 px-2"
         onClick={() => router.push("/time-sheets")}
       >
         <Image src={timeSheetIcon} alt="Home Icon" className="h-[22px]" />
         <h1 className="text-[16px] font-normal text-[#fff]  ">Time Sheets</h1>
       </div>
       <div
-        className="flex ml-[2.5rem] gap-4  mt-10 cursor-pointer "
+        className="flex gap-4  mt-4 cursor-pointer py-4 px-2 "
         onClick={() => router.push("/upcoming-events")}
       >
         <Image src={eventIcon} alt="Home Icon" className="h-[22px]" />
@@ -56,14 +70,14 @@ function Sidebar() {
       </div>
 
       <div
-        className="flex ml-[2.5rem] gap-4  mt-10 cursor-pointer"
+        className="flex gap-4  mt-4 cursor-pointer py-4 px-2"
         onClick={() => router.push("/notices")}
       >
         <Image src={noticeIcon} alt="Home Icon" className="h-[22px]" />
         <h1 className="text-[16px] font-normal text-[#fff]  ">Notices</h1>
       </div>
       <div
-        className="flex ml-[2.5rem] gap-4  mt-10 cursor-pointer"
+        className="flex gap-4  mt-4 cursor-pointer py-4 px-2"
         onClick={() => router.push("/schedule-and-help")}
       >
         <Image src={sheduleIcon} alt="Home Icon" className="h-[20px]" />
